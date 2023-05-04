4cb17a715e6c2c00b955ad2473ee7b82a1d2d5b8
V <mukhitov.aaa@list.ru>
2023-05-04 05:16:46 +0500

    Homework 7 models+ORM

--- core/news/admin.py
+++ core/news/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 
-from news.models import News
+from news.models import News, Category, Origin
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+class OriginAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
 
 admin.site.register(News)
+admin.site.register(Category)
+admin.site.register(Origin)
--- core/news/models.py
+++ core/news/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата публикации')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ManyToManyField('Category', null=True, verbose_name='Категория')
+    origin = models.ForeignKey('Origin', on_delete=models.PROTECT, null=True, verbose_name='Источник')
 
     def __str__(self):
         return self.title
@@ -16,3 +18,27 @@ class News(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+
+class Origin(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование источника')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Источник'
+        verbose_name_plural = 'Источники'
+        ordering = ['title']
