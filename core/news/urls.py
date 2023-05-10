f3e5505f50ea0b2f4fc0aee315599ef3fa175b83
V <mukhitov.aaa@list.ru>
2023-05-11 03:11:47 +0500

    Homework 8

--- core/news/urls.py
+++ core/news/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('test/', test)
+    path('', index, name='home'),
+    path('category/<int:category_id>', get_category, name='category'),
+    path('origin/<int:origin_id>', get_origin, name='origin')
 ]
--- core/news/views.py
+++ core/news/views.py
@@ -1,14 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from news.models import News
+from news.models import News, Category, Origin
 
 
 def index(request):
     news = News.objects.all()
-    context = {'news': news, 'title': 'Список новостей'}
+    categories = Category.objects.all()
+    context = {'news': news, 'categories': categories, 'title': 'Список новостей'}
     return render(request, template_name='news/index.html', context=context)
 
 
-def test(request):
-    return HttpResponse('<h1>Test Page</h1>')
+def get_category(request, category_id):
+    news = News.objects.filter(category=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
+
+
+def get_origin(request, origin_id):
+    news = News.objects.filter(origin_id=origin_id)
+    categories = Category.objects.all()
+    return render(request, 'news/category.html', {'news': news, 'categories': categories})
